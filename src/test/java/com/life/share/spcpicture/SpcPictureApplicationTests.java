27d751e5915fd55d40805a0478a99bf48f31a2be
luo.gl <luo.guolin@minglead.com>
2020-06-01 10:30:33 +0800

    初始化项目

---
+++ src/test/java/com/life/share/spcpicture/SpcPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life.share.spcpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcPictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
